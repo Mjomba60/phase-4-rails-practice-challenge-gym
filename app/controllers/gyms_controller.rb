5525fa16208769c5fdf258387cff3cb221a0537e
George Samuel <geosam2708@gmail.com>
2023-06-30 19:25:03 +0300

    updated action show

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,2 +1,13 @@
 class GymsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+    def show
+        render json: Gym.find(params[:id]), status: :ok
+    end
+
+    private
+
+    def record_not_found_response
+        render json: {error: "Gym not found"}, status: :not_found
+    end
 end
